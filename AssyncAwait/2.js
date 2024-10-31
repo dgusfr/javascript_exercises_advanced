e624b930628b47d8270d1a006b954c40d7a30f5c
dgusfr <diegogustavo.fr@gmail.com>
2024-10-30 21:41:21 -0300

    feat(struct)

--- AssyncAwait/2.js
+++ AssyncAwait/2.js
@@ -1,17 +1,15 @@
 async function fetchSequentialData() {
-  const url1 = "https://api.coindesk.com/v1/bpi/currentprice.json";
-  const url2 = "https://api.spacexdata.com/v4/launches/latest";
-  const url3 = "https://api.exchangerate-api.com/v4/latest/USD";
+  const urls = [
+    "https://api.coindesk.com/v1/bpi/currentprice.json",
+    "https://api.spacexdata.com/v4/launches/latest",
+    "https://api.exchangerate-api.com/v4/latest/USD",
+  ];
 
   try {
-    const data1 = await fetchData(url1);
-    console.log("Data 1:", data1);
-
-    const data2 = await fetchData(url2);
-    console.log("Data 2:", data2);
-
-    const data3 = await fetchData(url3);
-    console.log("Data 3:", data3);
+    for (const [index, url] of urls.entries()) {
+      const data = await fetchData(url);
+      console.log(`Data ${index + 1}:`, data);
+    }
   } catch (error) {
     console.error(`Error in fetching sequential data: ${error}`);
   }
